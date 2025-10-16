5df552ddbe3982d0a950b6053b0c569c28569c08
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2025-10-16 23:20:16 +0000

    Added the most important part... animated orpheus flag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container mx-auto my-10">
+	<h1 class="text-8xl font-semibold font-hero">Traffic</h1>
+	<p class="text-2xl font-medium">
+		You ship: traffic light design<br>
+        We ship: cool traffic light
+	</p>
+</div>
