9166c1e48bdb826a0afc3103d3237530a80e0a29
Arca Cengiz <arcaegecengiz@gmail.com>
2025-10-22 18:27:17 +0200

    Added session based auth

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,17 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const user = sqliteTable('user', {
+	id: text('id').primaryKey(),	// Slack ID
+});
+
+export const session = sqliteTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
+});
+
+export type Session = typeof session.$inferSelect;
+
+export type User = typeof user.$inferSelect;
