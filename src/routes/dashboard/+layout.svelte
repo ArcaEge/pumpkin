502c9e1d1a1a4246cf7144ff8bbbefeb5b5f47b7
Arca Cengiz <arcaegecengiz@gmail.com>
2025-10-23 19:59:37 +0200

    Created the sidebar layout

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
-    let { children, data } = $props();
+	let { children, data } = $props();
 </script>
 
-{data.user.name}
-<a href="/auth/logout">Logout</a>
-{@render children?.()}
+<div class="box-border flex h-[100svh] flex-row">
+	<div class="m-5 bg-amber-950 w-60">
+        <div class="flex flex-col gap-1">
+            <div>
+                {data.user.name}
+            </div>
+            <div>
+                <a href="/auth/logout">Logout</a>
+            </div>
+        </div>
+	</div>
+	<div class="grow overflow-auto">
+		{@render children?.()}
+        
+        a<br/>
+	</div>
+</div>
