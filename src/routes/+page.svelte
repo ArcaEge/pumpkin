0556bf323aeb9d45ab5442630371c524ae3f1c4b
Arca Cengiz <arcaegecengiz@gmail.com>
2025-10-23 16:02:31 +0200

    Changed Hack Club flag so it's only on the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,23 @@
 	import Footer from './Footer.svelte';
 </script>
 
+<a href="https://hackclub.com/">
+	<img
+		class="transition-transform hover:rotate-6"
+		style="
+					transform-origin: top left;
+					position: absolute;
+					top: -5px;
+					left: 10px;
+					border: 0;
+					width: 128px;
+					z-index: 999;
+				"
+		src="https://assets.hackclub.com/flag-orpheus-top.svg"
+		alt="Hack Club"
+	/>
+</a>
+
 <div
 	class="mt-40 flex w-full flex-col items-center justify-center px-10 md:mt-24 lg:mt-16 lg:flex-row"
 >
