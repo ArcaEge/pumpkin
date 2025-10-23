c050be6ace528dd5a3986fbe18cca29cfd7eb2f2
Arca Cengiz <arcaegecengiz@gmail.com>
2025-10-23 22:10:23 +0200

    Moved the sidebar to its own component

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
+	import Sidebar from './Sidebar.svelte';
 	let { children, data } = $props();
 </script>
 
 <div class="box-border flex h-[100svh] flex-row">
-	<div class="m-5 bg-amber-950 w-60">
-        <div class="flex flex-col gap-1">
-            <div>
-                {data.user.name}
-            </div>
-            <div>
-                <a href="/auth/logout">Logout</a>
-            </div>
-        </div>
-	</div>
+	<Sidebar user={data.user} />
 	<div class="grow overflow-auto">
 		{@render children?.()}
-        
-        a<br/>
 	</div>
 </div>
--- src/routes/dashboard/Sidebar.svelte
+++ src/routes/dashboard/Sidebar.svelte
@@ -1,3 +1,14 @@
 <script lang="ts">
-    
+	let { user } = $props();
 </script>
+
+<div class="m-5 p-5 w-60 bg-amber-950 border-amber-900 border-4 shadow-lg/20">
+	<div class="flex flex-col gap-1">
+		<div>
+			{user.name}
+		</div>
+		<div>
+			<a href="/auth/logout">Logout</a>
+		</div>
+	</div>
+</div>
