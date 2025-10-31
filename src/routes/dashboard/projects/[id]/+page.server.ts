fd75b3f8f35adbeb75f7809b9ed3586aadcf9b54
Arca Cengiz <arcaegecengiz@gmail.com>
2025-10-31 19:50:35 +0100

    Added devlog time check

--- src/routes/dashboard/projects/[id]/+page.server.ts
+++ src/routes/dashboard/projects/[id]/+page.server.ts
@@ -4,6 +4,9 @@ import { error, fail } from '@sveltejs/kit';
 import { eq, and, desc } from 'drizzle-orm';
 import type { Actions } from './$types';
 
+const DEVLOG_MIN_TIME = 5;
+const DEVLOG_MAX_TIME = 120;
+
 export async function load({ params }) {
 	let id: number = parseInt(params.id);
 
@@ -37,9 +40,16 @@ export async function load({ params }) {
 				id: devlog.id,
 				description: devlog.description,
 				timeSpent: devlog.timeSpent,
-				createdAt: devlog.createdAt,
+				createdAt: devlog.createdAt
 			};
-		})
+		}),
+		validationConstraints: {
+			timeSpent: {
+				min: DEVLOG_MIN_TIME,
+				max: DEVLOG_MAX_TIME,
+				currentMax: await getMaxDevlogTime(id)
+			}
+		}
 	};
 }
 
@@ -65,6 +75,7 @@ export const actions = {
 
 		const data = await request.formData();
 		const description = data.get('description');
+		const timeSpent = data.get('timeSpent');
 
 		if (
 			!description ||
@@ -72,18 +83,28 @@ export const actions = {
 			description.toString().trim().length > 1000
 		) {
 			return fail(400, {
-				fields: { description },
+				fields: { description, timeSpent },
 				invalid_description: true
 			});
 		}
 
-		// TODO: check if the time makes sense
+		if (
+			!timeSpent ||
+			!parseInt(timeSpent.toString()) ||
+			parseInt(timeSpent.toString()) < DEVLOG_MIN_TIME ||
+			parseInt(timeSpent.toString()) > (await getMaxDevlogTime(id))
+		) {
+			return fail(400, {
+				fields: { description, timeSpent },
+				invalid_timeSpent: true
+			});
+		}
 
 		await db.insert(devlog).values({
 			userId: locals.user.id,
 			projectId: queriedProject.id,
 			description: description.toString().trim(),
-			timeSpent: 60,
+			timeSpent: parseInt(timeSpent.toString()),
 			createdAt: new Date(Date.now()),
 			updatedAt: new Date(Date.now())
 		});
@@ -91,3 +112,24 @@ export const actions = {
 		return { success: true };
 	}
 } satisfies Actions;
+
+async function getMaxDevlogTime(id: number) {
+	const queriedDevlogArray = await db
+		.select()
+		.from(devlog)
+		.where(and(eq(devlog.projectId, id), eq(devlog.deleted, false)))
+		.orderBy(desc(devlog.createdAt))
+		.limit(1);
+
+	let devlogCurrentMaxTime;
+
+	if (queriedDevlogArray.length == 0) {
+		devlogCurrentMaxTime = DEVLOG_MAX_TIME;
+	} else {
+		let diff = new Date(Date.now()).valueOf() - queriedDevlogArray[0].createdAt.valueOf();
+
+		devlogCurrentMaxTime = Math.min(Math.floor(Math.abs(diff / 1000 / 60)), DEVLOG_MAX_TIME);
+	}
+
+	return devlogCurrentMaxTime;
+}
--- src/routes/dashboard/projects/[id]/+page.svelte
+++ src/routes/dashboard/projects/[id]/+page.svelte
@@ -3,9 +3,25 @@
 	import relativeDate from 'tiny-relative-date';
 	import type { PageProps } from './$types';
 
+	const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
+
 	let { data, form }: PageProps = $props();
 	let sortDropdownValue = $state('descending');
 	let sortDevlogsAscending = $derived.by(() => sortDropdownValue == 'ascending');
+
+	let timeSpent = $state(
+		form?.fields?.timeSpent?.toString()
+			? (parseInt(form?.fields?.timeSpent?.toString()) ?? data.validationConstraints.timeSpent.min)
+			: data.validationConstraints.timeSpent.min
+	);
+
+	function onchange() {
+		timeSpent = clamp(
+			timeSpent,
+			data.validationConstraints.timeSpent.min,
+			data.validationConstraints.timeSpent.currentMax
+		);
+	}
 </script>
 
 <h1 class="mt-5 mb-2 font-hero text-3xl font-medium">{data.project.name}</h1>
@@ -47,28 +63,71 @@
 	</div>
 
 	<h3 class="mt-6 mb-1 text-xl font-semibold">Add entry</h3>
-	<form method="POST" class="flex flex-col gap-3">
-		<div>
-			<label class="flex flex-col gap-1">
-				Description*
-				<textarea
-					name="description"
-					placeholder="Describe what you changed"
-					class="border-3 border-dashed border-amber-900 bg-amber-950 ring-amber-900 placeholder:text-amber-900 active:ring-3"
-					>{form?.fields?.description ?? ''}</textarea
-				>
-			</label>
-			{#if form?.invalid_description}
-				<p class="mt-1 text-sm">Invalid description, must be between 20 and 1000 characters</p>
-			{/if}
-		</div>
-		<button
-			type="submit"
-			class="cursor-pointer bg-amber-800 p-2 outline-amber-50 transition-colors hover:bg-amber-700 hover:outline-3"
-		>
-			Add journal entry!
-		</button>
-	</form>
+	{#if data.validationConstraints.timeSpent.currentMax >= data.validationConstraints.timeSpent.min}
+		<form method="POST" class="flex flex-col gap-3">
+			<div class="flex flex-col gap-2">
+				<label class="flex flex-col gap-1">
+					Time spent (minutes)
+					<div class="flex gap-5">
+						<div>
+							<input
+								name="timeSpent"
+								type="number"
+								bind:value={timeSpent}
+								step="1"
+								min={data.validationConstraints.timeSpent.min}
+								max={data.validationConstraints.timeSpent.max}
+								{onchange}
+								class="w-25 border-3 border-dashed border-amber-900 bg-amber-950 ring-amber-900 placeholder:text-amber-900 active:ring-3"
+							/>
+						</div>
+						<input
+							name="timeSpent"
+							type="range"
+							class="grow accent-amber-500"
+							bind:value={timeSpent}
+							step="1"
+							min="0"
+							{onchange}
+							max={data.validationConstraints.timeSpent.max}
+						/>
+					</div>
+					<p class="text-sm">
+						The minimum journal time is {data.validationConstraints.timeSpent.min} minutes, the maximum
+						is
+						{data.validationConstraints.timeSpent.max ==
+						data.validationConstraints.timeSpent.currentMax
+							? ''
+							: 'currently'}
+						{data.validationConstraints.timeSpent.currentMax}
+					</p>
+				</label>
+				<label class="flex flex-col gap-1">
+					Description
+					<textarea
+						name="description"
+						placeholder="Describe what you changed"
+						class="border-3 border-dashed border-amber-900 bg-amber-950 ring-amber-900 placeholder:text-amber-900 active:ring-3"
+						>{form?.fields?.description ?? ''}</textarea
+					>
+				</label>
+				{#if form?.invalid_description}
+					<p class="mt-1 text-sm">Invalid description, must be between 20 and 1000 characters</p>
+				{/if}
+			</div>
+			<button
+				type="submit"
+				class="cursor-pointer bg-amber-800 p-2 outline-amber-50 transition-colors hover:bg-amber-700 hover:outline-3"
+			>
+				Add journal entry!
+			</button>
+		</form>
+	{:else}
+		<p>
+			You must work for at least {data.validationConstraints.timeSpent.min -
+				data.validationConstraints.timeSpent.currentMax} more minutes to journal! [insert orpheus drawing]
+		</p>
+	{/if}
 {/if}
 
 <div class="mt-6 mb-5 flex flex-col gap-3">
@@ -99,6 +158,7 @@
 		{#each sortDevlogsAscending ? [...data.devlogs].reverse() : data.devlogs as devlog}
 			<div
 				class="relative flex flex-col border-3 border-dashed border-amber-900 bg-amber-950 p-3 shadow-lg/20 transition-all"
+				id={`devlog-${devlog.id}`}
 			>
 				<p class="mb-0.5 text-sm opacity-90">
 					<abbr title={`${devlog.createdAt.toUTCString()}`}>
@@ -112,7 +172,7 @@
 				{#if data.project.userId == data.user.id}
 					<div class="mt-1 flex flex-row gap-1">
 						<a
-							href={`/dashboard/projects/${data.project.id}/edit`}
+							href={`/dashboard/projects/${data.project.id}/devlog/${devlog.id}/edit`}
 							class="flex cursor-pointer flex-row gap-1 bg-amber-800 p-1.5 text-xs outline-amber-50 transition-colors hover:bg-amber-700 hover:outline-2"
 						>
 							<SquarePen size={16} />
