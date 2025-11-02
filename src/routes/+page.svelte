b61330c37959defae417dc0da9408fc085399e9a
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2025-11-02 22:03:43 +0000

    Updated the front page to add the pumpkin stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,196 @@
 <script lang="ts">
-	import leaf from '$lib/assets/leaf.png';
-
 	import Button from '$lib/components/Button.svelte';
 	import Accordion from '$lib/components/Accordion.svelte';
 	import Rules from './Rules.svelte';
 	import Prizes from './Prizes.svelte';
 	import Footer from './Footer.svelte';
 
+	import model from '$lib/assets/pumpkin.3mf?url';
+
 	let { data } = $props();
+
+	import * as THREE from 'three';
+	import { ThreeMFLoader } from 'three/examples/jsm/Addons.js';
+	import { OrbitControls } from 'three/examples/jsm/Addons.js';
+	import { onMount } from 'svelte';
+
+	// Necessary for camera/plane rotation
+	let degree = Math.PI / 180;
+
+	// Create scene
+	const scene = new THREE.Scene();
+
+	onMount(() => {
+		if (!model) {
+			return;
+		}
+
+		let canvas = document.querySelector(`#canvas`);
+
+		if (!canvas) {
+			return;
+		}
+
+		const renderer = new THREE.WebGLRenderer({
+			canvas,
+			antialias: true,
+			alpha: true
+		});
+
+		renderer.setClearColor(0xffffff, 0);
+		renderer.setPixelRatio(window.devicePixelRatio);
+
+		renderer.shadowMap.enabled = true;
+		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+		// There's no reason to set the aspect here because we're going
+		// to set it every frame anyway so we'll set it to 2 since 2
+		// is the the aspect for the canvas default size (300w/150h = 2)
+		const camera = new THREE.PerspectiveCamera(40, 2, 1, 1000);
+		camera.rotation.x = -45 * degree;
+
+		// Add controls, targetting the same DOM element
+		let controls = new OrbitControls(camera, renderer.domElement);
+		controls.target.set(0, 0, 0);
+		controls.rotateSpeed = 0.3;
+		controls.enablePan = false;
+		controls.dampingFactor = 0.1;
+		controls.enableDamping = true;
+		controls.autoRotate = true;
+		controls.autoRotateSpeed = 2;
+		controls.update();
+
+		// Lighting
+		const hemisphere = new THREE.HemisphereLight(0xffffff, 0xffffff, 1.1);
+		scene.add(hemisphere);
+
+		const directional = new THREE.DirectionalLight(0xffffff, 1.1);
+		directional.castShadow = true;
+		directional.shadow.mapSize.width = 2048;
+		directional.shadow.mapSize.height = 2048;
+		scene.add(directional);
+
+		const directional2 = new THREE.DirectionalLight(0xffffff, 1.1);
+		directional2.castShadow = true;
+		directional2.shadow.mapSize.width = 2048;
+		directional2.shadow.mapSize.height = 2048;
+		scene.add(directional2);
+
+		function resizeCanvasToDisplaySize() {
+			const canvas = renderer.domElement;
+			const width = canvas.clientWidth;
+			const height = canvas.clientHeight;
+			if (canvas.width !== width || canvas.height !== height) {
+				// you must pass false here or three.js sadly fights the browser
+				renderer.setSize(width, height, false);
+				renderer.setPixelRatio(window.devicePixelRatio);
+				camera.aspect = width / height;
+				camera.updateProjectionMatrix();
+			}
+		}
+
+		function parseObject(object: THREE.Group<THREE.Object3DEventMap>) {
+			object = object as THREE.Group<THREE.Object3DEventMap> & { children: THREE.Mesh[] };
+
+			object.rotation.x = THREE.MathUtils.degToRad(-90);
+			object.rotation.z = THREE.MathUtils.degToRad(-25);
+
+			const aabb = new THREE.Box3().setFromObject(object);
+			const center = aabb.getCenter(new THREE.Vector3());
+
+			object.position.x += object.position.x - center.x;
+			object.position.y += object.position.y - center.y;
+			object.position.z += object.position.z - center.z;
+
+			controls.reset();
+
+			var box = new THREE.Box3().setFromObject(object);
+			const size = new THREE.Vector3();
+			box.getSize(size);
+			const largestDimension = Math.max(size.x, size.y, size.z);
+
+			camera.position.z = largestDimension * 1.3;
+			camera.position.y = 0;
+
+			directional.position.set(largestDimension * 2, largestDimension * 2, largestDimension * 2);
+			directional2.position.set(-largestDimension * 2, largestDimension * 2, -largestDimension * 2);
+
+			camera.near = largestDimension * 0.001;
+			camera.far = largestDimension * 10;
+			camera.updateProjectionMatrix();
+
+			const edgeLines: { lines: THREE.LineSegments; mesh: THREE.Mesh }[] = [];
+
+			object.traverse(function (child) {
+				child.castShadow = true;
+				child.receiveShadow = true;
+
+				const mesh = child as THREE.Mesh;
+
+				const material = mesh.material;
+
+				if (Array.isArray(material)) {
+					material.forEach((mat) => {
+						mat.transparent = true;
+						mat.side = THREE.DoubleSide;
+						mat.opacity = 0.9;
+						mat.needsUpdate = true;
+					});
+				} else if (material instanceof THREE.Material) {
+					material.transparent = true;
+					material.side = THREE.DoubleSide;
+					material.opacity = 0.9;
+					material.needsUpdate = true;
+				}
+
+				const edges = new THREE.EdgesGeometry(mesh.geometry);
+				const lines = new THREE.LineSegments(
+					edges,
+					new THREE.LineBasicMaterial({
+						color: 0xf3dcc6,
+						linewidth: 1,
+						polygonOffset: true,
+						polygonOffsetFactor: -1,
+						polygonOffsetUnits: -1
+					})
+				);
+
+				lines.position.copy(mesh.position);
+				lines.rotation.copy(mesh.rotation);
+
+				edgeLines.push({ lines, mesh });
+			});
+
+			// Have to do it this way to avoid infinite recursion
+			edgeLines.forEach(({ lines, mesh }) => {
+				mesh.add(lines);
+			});
+
+			scene.add(object);
+		}
+
+		var loader = new ThreeMFLoader();
+
+		loader.load(
+			model,
+			parseObject,
+			(xhr) => {
+				// TODO: loading slider
+				console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+
+		const animate = function () {
+			requestAnimationFrame(animate);
+			controls.update();
+			renderer.render(scene, camera);
+			resizeCanvasToDisplaySize();
+		};
+		animate();
+	});
 </script>
 
 <a href="https://hackclub.com/">
@@ -27,11 +210,9 @@
 	/>
 </a>
 
-<div
-	class="mt-40 flex w-full flex-col items-center justify-center px-10 md:mt-24 lg:mt-16 lg:flex-row"
->
-	<div>
-		<h1 class="font-hero text-5xl sm:text-6xl md:text-7xl">Pumpkin</h1>
+<div class="flex w-full flex-col items-center justify-center px-10 lg:flex-row">
+	<div class="mt-40">
+		<h1 class="font-hero text-4xl sm:text-5xl md:text-6xl">Pumpkin</h1>
 		<p class="my-3 text-xl font-medium">
 			You ship: pumpkin design<br />
 			We ship: very cool pumpkin
@@ -42,11 +223,12 @@
 			<Button text="Login with Slack" href="/auth/slack" />
 		{/if}
 	</div>
-	<img
-		src={leaf}
-		alt="Leaf CAD model"
-		class="h-100 drop-shadow-md transition-all hover:drop-shadow-2xl hover:drop-shadow-amber-600/10 active:hover:drop-shadow-amber-600/20 lg:ml-20"
-	/>
+	<div class="h-80 w-80 md:w-100 flex flex-col mt-12">
+		<canvas class="h-full w-full" id={`canvas`}></canvas>
+		<p class="text-sm w-full text-center mt-2">
+			interact with me!
+		</p>
+	</div>
 </div>
 
 <div class="mt-24 flex flex-col items-center justify-center px-10">
@@ -54,7 +236,7 @@
 	<div class="w-full max-w-2xl">
 		<p class="mt-3 max-w-2xl">
 			<strong>Ever wanted to own a cool pumpkin?</strong> Now, you can do so! All you need to do is spend
-			10 hours designing a pumpkin in CAD and we'll one to you from HQ Vermont, for free.
+			10 hours designing a pumpkin in CAD and we'll ship one right to your house, completely for free.
 		</p>
 	</div>
 </div>
--- src/routes/Prizes.svelte
+++ src/routes/Prizes.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	import prize01 from '$lib/assets/prizes/canada.jpg';
-	import prize02 from '$lib/assets/prizes/canadaplusplus.jpg';
-	import prize03 from '$lib/assets/prizes/mini-tree.jpg';
+	import prize01 from '$lib/assets/prizes/pumpkin-small.jpg';
+	import prize02 from '$lib/assets/prizes/pumpkin-plastic.jpg';
+	import prize03 from '$lib/assets/prizes/pumpkin-mystery.jpg';
 
 	import ImageCard from '$lib/components/ImageCard.svelte';
 </script>
 
 <div class="mt-16 flex flex-col items-center justify-center px-10">
 	<h1 class="mb-5 text-center text-3xl font-bold sm:text-4xl">Prizes</h1>
-	<p>You'll get a couple of these leaves!</p>
+	<p>You'll get one of these pumpkins!</p>
 	<div class="mt-5 flex flex-col gap-4 md:flex-row">
 		<ImageCard
-			title="Canada!"
-			text="Common Canada flag-looking leaf ∙ 10 hours"
+			title="Small one"
+			text="Your typical pumpkin but it's pretty small ∙ 10 hours"
 			imgsrc={prize01}
 		/>
 		<ImageCard
-			title="Canada++"
-			text="Like the Canada one but with more points ∙ 15 hours"
+			title="Plastic"
+			text="Reusable! Doesn't taste as nice though... ∙ 15 hours"
 			imgsrc={prize02}
 		/>
 		<ImageCard
-			title="Mini tree"
-			text="Not sure how else to describe it... ∙ 20 hours"
+			title="Mystery!"
+			text="It's a mystery!! ∙ 20 hours"
 			imgsrc={prize03}
 		/>
 	</div>
